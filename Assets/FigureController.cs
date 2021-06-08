b1bd60bceb27c1a1632acc87ce9623616f050b57
Borisov Alexandr <borisov.av.dev@gmail.com>
2021-06-09 02:27:40 +0300

    basic control setup

--- Assets/FigureController.cs
+++ Assets/FigureController.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class FigureController : MonoBehaviour
 {
-    void Start()
+    public float mouseSpeedMultiplier = 3;
+    public float dragThreshold = 0.1f;
+
+    private Transform _camTf;
+    private float _mDeltaX;
+    private float _mDeltaY;
+    private bool _isMousePressed;
+
+    private void Start()
     {
-        
+        _camTf = Camera.main.transform;
     }
 
-    void Update()
+    private void OnMouseDown()
     {
-        
+        _isMousePressed = true;
+    }
+
+    private void OnMouseUp()
+    {
+        _isMousePressed = false;
+    }
+
+    private void OnMouseDrag()
+    {
+        _mDeltaX = Input.GetAxis("Mouse X") * mouseSpeedMultiplier;
+        _mDeltaY = Input.GetAxis("Mouse Y") * mouseSpeedMultiplier;
+    }
+
+    private void Update()
+    {
+        HandleRotation();
+    }
+
+    private void HandleRotation()
+    {
+        if (!_isMousePressed)
+            return;
+        if (Mathf.Abs(_mDeltaX) < dragThreshold && Mathf.Abs(_mDeltaY) < dragThreshold)
+            return;
+
+        if (Input.GetKey(KeyCode.LeftControl))
+            transform.RotateAround(transform.position, _camTf.up, -_mDeltaX);
+        else
+            transform.RotateAround(transform.position, _camTf.right, _mDeltaY);
     }
 }
