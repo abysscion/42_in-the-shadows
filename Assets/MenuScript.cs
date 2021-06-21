319d579ae3796cbe846eb2a8e07e4e46f2bce5f9
Borisov Alexandr <borisov.av.dev@gmail.com>
2021-06-22 02:39:17 +0300

    Working menu, ddol with gamemanager, etc.

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public void OnButtonClick_StartGame()
+    {
+        GameManager.Instance.StartNewGame();
+    }
+
+    public void OnButtonClick_ExitGame()
+    {
+        GameManager.Instance.ExitGame();
+    }
+
+    public void OnButtonClick_OpenOptions()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene((int)GameScenes.Level_1);
+    }
+
+    private void Awake()
+    {
+        Instance = Instance ? Instance : this;
+        Application.targetFrameRate = 60;
+        SceneManager.LoadScene((int)GameScenes.Menu);
+    }
+
+    public enum GameScenes
+    {
+        DDOL,
+        Map,
+        Menu,
+        Level_1,
+        Level_2
+    }
+}
